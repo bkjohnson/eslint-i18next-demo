814c7ab61cc1e32d4c8366543973b97eceef309d
Brooks Johnson <brooks.johnson@pm.me>
2022-07-12 09:27:20 -0700

    Add initial stencil setup

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'eslint-i18next-demo',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
